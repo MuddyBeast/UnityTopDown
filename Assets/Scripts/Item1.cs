3ec1d21b0250ec55c39982ac9b19c9dc4827daa7
Ezlo42 <tobias424242@hotmail.com>
2016-03-30 15:22:37 +0200

    Added files via upload

---
+++ Assets/Scripts/Item1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item1 : MonoBehaviour
+{
+	public GameObject leaf;
+	public GameObject projectile;
+	Vector2 direction;
+
+	float currentTime = 0.0f;
+	public float fireRate = 0.6f;
+
+	// Update is called once per frame
+	void Update()
+	{
+		currentTime += Time.deltaTime;
+
+		if (currentTime > fireRate && Input.GetKey(KeyCode.Alpha1))
+		{
+			currentTime = 0f;
+			leaf = GameObject.FindGameObjectWithTag ("leaf");
+
+			GameObject instance;
+			instance = Instantiate(projectile, leaf.transform.position, leaf.transform.rotation) as GameObject;
+		}
+	}
+}
---
+++ Assets/Scripts/VisionProjectile.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisionProjectile : MonoBehaviour {
+
+	public float speed = 0.5f;
+	public float lifeSpann = 0.8f; 
+	float currentTime = 0.0f; 
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		currentTime += Time.deltaTime;
+		transform.Translate (Vector2.up * speed * Time.deltaTime);
+
+
+		//Attach modified visionscript HERE PLEASE.  
+
+		if (currentTime > lifeSpann) 
+		{
+			Destroy (gameObject);
+		}
+	}
+}
