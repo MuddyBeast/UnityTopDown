e800f9c10e9e6fa486e02e4583a81ed8df07aa1f
Mextrin <seba3552@hotmail.com>
2016-03-30 14:58:25 +0200

    Button
    Added:
    - Button
    - A scene for Mextrin

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour
+{
+    bool active;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            active = !active;
+        }
+    }
+}
