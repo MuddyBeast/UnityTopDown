0c34b1c9982e797b26624f6591ab5413f622d93a
Mextrin <seba3552@hotmail.com>
2016-03-31 17:25:38 +0200

    Added files via upload

---
+++ Assets/Prefabs/Button.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Button : MonoBehaviour
+{
+    public bool active;
+    public Color activeColor, inactiveColor;
+    public bool multipleSwitches;
+    bool canChange;
+
+    SpriteRenderer buttonColor;
+
+    void Start()
+    {
+        buttonColor = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+    
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (multipleSwitches)
+            {
+                active = !active;
+
+                if (active)
+                    buttonColor.color = activeColor;
+
+                else
+                    buttonColor.color = inactiveColor;
+            }
+
+            else
+            {
+                if (!active)
+                    active = !active;
+
+            }
+        }
+    }
+}
---
+++ Assets/Prefabs/MovementScript.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementScript : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public Sprite sprite;
+    private SpriteRenderer renderer;
+
+    public float startingSpeed;
+    float currentSpeed;
+    public float movementSpeedIncrease;
+    public float maxSpeed;
+    public float runSpeed;
+    bool isRunning;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        renderer = GetComponent<SpriteRenderer>();
+        currentSpeed = startingSpeed;
+    }
+
+
+    void FixedUpdate()
+    {
+        //Debug.Log(currentSpeed);
+        //if (Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
+        //{
+        //    rb.velocity = Vector2.Lerp(rb.velocity, Vector2.zero, 5f * Time.deltaTime);
+        //    currentSpeed = startingSpeed;
+        //}
+        Movement();
+
+    }
+
+    void Movement()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(KeyCode.LeftShift))
+            isRunning = true;
+        else
+            if (isRunning == true)
+                isRunning = false;
+
+        if (isRunning)
+            rb.velocity = new Vector2(h * (currentSpeed + runSpeed), v * (currentSpeed + runSpeed));
+        else
+            rb.velocity = new Vector2(h * currentSpeed, v * currentSpeed);
+
+        if (maxSpeed > currentSpeed)
+        {
+            currentSpeed += movementSpeedIncrease;
+            if (currentSpeed > maxSpeed)
+                currentSpeed = maxSpeed;
+        }
+        if (h == 0 && v == 0)
+            currentSpeed = startingSpeed;
+
+        //if (v > 0)
+        //    renderer.sprite = sprites[0];
+
+        //if (v < 0)
+        //    renderer.sprite = sprites[1];
+
+        //if (Input.GetAxisRaw("Horizontal") > 0)
+        //{
+        //    transform.Translate(Vector2.right * currentSpeed);
+        //    if (maxSpeed > currentSpeed)
+        //    {
+        //        currentSpeed += movementSpeedIncrease * Time.deltaTime;
+        //        if (currentSpeed > maxSpeed)
+        //            currentSpeed = maxSpeed;
+        //    }
+        //}
+        //else if (Input.GetAxisRaw("Horizontal") < 0)
+        //{
+        //    transform.Translate(Vector2.left * currentSpeed);
+        //    if (maxSpeed > currentSpeed)
+        //    {
+        //        currentSpeed += movementSpeedIncrease * Time.deltaTime;
+        //        if (currentSpeed > maxSpeed)
+        //            currentSpeed = maxSpeed;
+        //    }
+        //}
+        //else
+        //{
+        //    rb.velocity = Vector2.Lerp(rb.velocity, Vector2.zero, 5f * Time.deltaTime);
+        //    currentSpeed = startingSpeed;
+        //}
+    }
+}
